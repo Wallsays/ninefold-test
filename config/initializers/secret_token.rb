c3d60f41d45a1e2ee80a6d8230a24ba5a8e16d1f
Dis <sr_5@inbox.ru>
2014-01-02 15:41:44 +0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NinefoldTest::Application.config.secret_key_base = '4d5ccc6e8974c47a7fa0a7828031428ceb545794c6c9ac6da9c1fa07e8a53d0f997edda9e05a5d3c8e69f16129c84d22e611d521c3407cee2139425116f0019d'
